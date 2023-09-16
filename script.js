e04f25e7ec0cb13aa0dba5ec3038b3300516856d
Anjal Patel <patel.anjal567@gmail.com>
2023-09-16 15:37:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var count = 0;
+
+function changeCount(num){
+count += num;
+
+document.getElementById("count").innerHTML = count; /* This is always for displaying document.getElementById is always for displaying */
+}
